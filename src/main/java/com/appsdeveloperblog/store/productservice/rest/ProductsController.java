5c42f10584a71535aea5382222b85f8e22ee0cbd
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2025-01-31 18:31:20 -0500

    Add Get method controller.

--- src/main/java/com/appsdeveloperblog/store/productservice/rest/ProductsController.java
+++ src/main/java/com/appsdeveloperblog/store/productservice/rest/ProductsController.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.store.productservice.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class ProductsController {
     @PostMapping
     public String createProduct() {
         return "HTTP POST Handled";
+    }
+
+    @GetMapping
+    public String getProduct() {
+        return "HTTP GET Handled";
     }
 
 
