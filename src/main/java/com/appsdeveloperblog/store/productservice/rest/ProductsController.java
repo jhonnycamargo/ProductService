7913c1f04074ee3ceb6562b4f1d2982c300c66a7
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2025-01-31 18:30:11 -0500

    Add Post controller.

--- src/main/java/com/appsdeveloperblog/store/productservice/rest/ProductsController.java
+++ src/main/java/com/appsdeveloperblog/store/productservice/rest/ProductsController.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.store.productservice.rest;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductsController {
 
-    @RequestMapping("/get")
+    @PostMapping
     public String createProduct() {
-        return "Product details";
+        return "HTTP POST Handled";
     }
+
+
 }
